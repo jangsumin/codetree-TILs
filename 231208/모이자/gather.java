07148258faf3af33ea08cce1a3b0fa619e86d7e8
장수민 <jsm0726@naver.com>
2023-12-08 14:23:52 +0900

    Title: 모이자, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231208/모이자/gather.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                sum += Math.abs(j - i) * arr[j];
+            }
+            result = Math.min(result, sum);
+        }
+
+        System.out.println(result);
+    }
+}
